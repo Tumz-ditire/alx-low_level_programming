4a5050445bd1861a133fd082bcb78800f64b91be
Boitumelo <boitumeloditire@ymail.com>
2023-02-16 16:59:40 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print the code using puts
+ *
+ * return: 0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
