09d4a463a3c61f449eecbc2a0fc969e5b568a0f8
Boitumelo <boitumeloditire@ymail.com>
2023-02-21 12:20:02 -0800

    Updated _putchar.c file

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - write the character c.
+ * @c: The character to print
+ * Return: 1 (Success)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - print putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write("_putchar\n");
+	return (0);
+}
