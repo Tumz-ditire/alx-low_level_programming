2a6cac9b62a96b9763b5223e823115c7c1a92a2e
Boitumelo <boitumeloditire@ymail.com>
2023-02-22 06:01:04 -0800

    checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: character to be checked
+ * Return: 1 (Success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+
+	}
+}
