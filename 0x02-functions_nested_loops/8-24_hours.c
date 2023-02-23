3921b90b3f937095d070310050c35a3b585ac672
Boitumelo <boitumeloditire@ymail.com>
2023-02-23 08:24:44 -0800

    prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,20 +8,16 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	h = 0;
-	while (h <= 24)
+	for (h = 0; h <= 23; h++)
 	{
-		m = 0;
-		while (m <= 60)
+		for (m = 0; m <= 59; m++)
 		{
-			_putchar((h / 10) + 0);
-			_putchar((h % 10) + 0);
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar((m / 10) + 0);
-			_putchar((m % 10) + 0);
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
-		m++;
 		}
-	h++;
 	}
 }
