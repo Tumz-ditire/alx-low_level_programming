51fae2d153a049668d19ce49def380c440977165
Boitumelo <boitumeloditire@ymail.com>
2023-02-24 13:04:55 -0800

    function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - function that prints the numbers, from 0 to 9.
- * excluding 2 and 4
+ * excluding 2 and 4, add a new line.
  * Return: Always 0
  */
 void print_most_numbers(void)
