3ffa168344f2e8d86910db7b28c4c5805805101a
Boitumelo <boitumeloditire@ymail.com>
2023-02-24 12:59:25 -0800

    function that prints 10 times the numbers, from 0 to 14

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9.
+ * excluding 2 and 4
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
