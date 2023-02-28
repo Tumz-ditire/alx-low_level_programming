6f236c0a4549f9ac0035f186fd63e98d3987f672
Boitumelo <boitumeloditire@ymail.com>
2023-02-28 15:45:21 -0800

    generates random valid passwords for the program

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - program that generates random valid passwords,
+ * for the program 101-crackme.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int passwrd[100];
+	int a, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (a = 0; a < 100; a++)
+	{
+		passwrd[a] = rand() % 78;
+		sum += (passwrd[a] + '0');
+		putchar(passwrd[a] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
