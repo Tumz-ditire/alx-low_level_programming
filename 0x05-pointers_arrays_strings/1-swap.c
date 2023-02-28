ac1d08db58d973361eeceb752a6f67786afd1869
Boitumelo <boitumeloditire@ymail.com>
2023-02-27 19:36:58 -0800

    swapping the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *a + *b ;
+	*b = *a - *b;
+	*a = *a - *b;
+}
