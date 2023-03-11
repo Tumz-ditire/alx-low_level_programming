fe7a93575a2f7c0a952afa3ba61bb5e656834a5c
Boitumelo <boitumeloditire@ymail.com>
2023-03-11 12:36:41 -0800

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
